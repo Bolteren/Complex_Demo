fec7df3de70c2d476633bbaf91f6a4bdf147e60b
BolterHell@gmail.com <BolterHell@gmail.com>
2020-05-10 22:23:22 +0500

    Реализация завершена.

---
+++ Complex_Demo/Complex.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Complex_Demo
+{
+    internal class Complex
+    {
+        private double iReal, iImag;
+        public Complex(double iReal, double iImag)
+        {
+            this.iReal = iReal;
+            this.iImag = iImag;
+        }
+        public static Complex operator +(Complex z1, Complex z2)
+            => new Complex(z1.iReal + z2.iReal, z1.iImag + z2.iImag);
+        public static Complex operator -(Complex z1, Complex z2)
+            => new Complex(z1.iReal - z2.iReal, z1.iImag - z2.iImag);
+        public static Complex operator +(double n, Complex z1)
+            => new Complex(z1.iReal + n, z1.iImag);
+        public static Complex operator +(Complex z1, double n)
+            => n + z1;
+        public static Complex operator -(double n, Complex z1)
+            => new Complex(z1.iReal - n, z1.iImag);
+        public static Complex operator -(Complex z1, double n)
+            => new Complex(n - z1.iReal, z1.iImag);
+        public static Complex operator -(Complex z1)
+            => new Complex(-z1.iReal, -z1.iImag);
+        public static bool operator ==(Complex z1, Complex z2)
+            => z1.iReal == z2.iReal && z1.iImag == z2.iImag;
+        public static bool operator !=(Complex z1, Complex z2)
+            => z1.iReal != z2.iReal || z1.iImag != z2.iImag;
+        public static Complex operator *(Complex z1, Complex z2) 
+            => new Complex(z1.iReal * z2.iReal - z1.iImag * z2.iImag, z1.iReal * z2.iImag + z2.iReal * z1.iImag);
+        public static Complex operator *(double n, Complex z)
+            => new Complex(z.iReal * n, n * z.iImag);
+        public static Complex operator *(Complex z, double n)
+            => n * z;
+        public static Complex operator /(Complex z1, Complex z2)
+        {
+            double imagen, real;
+            real = (z1.iReal * z2.iReal + z1.iImag * z2.iImag) / (Math.Pow(z2.iReal,2) + Math.Pow(z2.iImag, 2));
+            imagen = (z2.iReal * z1.iImag - z1.iReal * z2.iImag) / (Math.Pow(z2.iReal, 2) + Math.Pow(z2.iImag, 2));
+            return new Complex(real, imagen);
+        }
+        public static Complex operator /(Complex z1, double n)
+            => new Complex((z1.iReal * n) / Math.Pow(n, 2), (n * z1.iImag) / Math.Pow(n, 2));
+        public static Complex operator /(double n, Complex z1) //TODU: Поправить! Так это не работает!!!!
+            => new Complex( (n*z1.iReal)/(Math.Pow(z1.iReal,2) + Math.Pow(z1.iImag, 2)) , (-n * z1.iImag)/(Math.Pow(z1.iReal, 2) + Math.Pow(z1.iImag, 2)));
+
+        public override string ToString()
+        {
+            string complex_to_string; 
+            if (this.iImag < 0)
+                complex_to_string = $"[{iReal} - {-iImag}i]";
+            else complex_to_string = $"[{iReal} + {iImag}i]";
+            return complex_to_string;
+        }
+    }
+}
--- Complex_Demo/Program.cs
+++ Complex_Demo/Program.cs
@@ -6,7 +6,23 @@ namespace Complex_Demo
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
+            Complex z1 = new Complex(2, 3);
+            Complex z2 = new Complex(-1, 1);
+            Complex z3 = new Complex(4, 6);
+            int n = 2;
+            Console.WriteLine("Сумма комплексных чисел\t\t\t\t\t{0} + {1} = {2}", z1, z2, z1 + z2);
+            Console.WriteLine("Разность комплексных чисел\t\t\t\t{0} - {1} = {2}", z1, z2, z1 - z2);
+            Console.WriteLine("Сумма действительного и комплексного чисела\t\t{0} + {1} = {2}", n, z1, n + z1);
+            Console.WriteLine("Сумма комплексного и действительного чисела\t\t{0} + {1} = {2}", z1, n, z1 + n);
+            Console.WriteLine("Разность комплексного и действительного чисела\t\t{0} - {1} = {2}", z2, n, z2 - n);
+            Console.WriteLine("Разность действительного и комплексных чисел\t\t{0} - {1} = {2}", n, z2, n - z2);
+            Console.WriteLine("Умножение комплексных чисел\t\t\t\t{0} * {1} = {2}", z1, z2, z1 * z2);
+            Console.WriteLine("Умножение действительного и комплексного чисела\t\t{0} * {1} = {2}", n, z1, n * z1);
+            Console.WriteLine("Умножение комплексного и действительного чисела\t\t{0} * {1} = {2}", z1, n, z1 * n);
+            Console.WriteLine("Деление комплексных чисел\t\t\t\t{0} / {1} = {2}", z3, z1, z3 / z1);
+            Console.WriteLine("Деление комплексного и натурального чисела\t\t{0} / {1} = {2}", z3, n, z3 / n);
+            Console.WriteLine("Деление действительного и комплексного чисела\t\t{0} / {1} = {2}", n, z2, n / z2);
+            Console.ReadKey();
         }
     }
 }
